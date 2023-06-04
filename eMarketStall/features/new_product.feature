50c39b87722df91627772a250a0dd195c61633bd
iorenzolacopino <iacopino.1965855@studenti.uniroma1.it>
2023-06-04 18:30:18 +0200

    Due nuove features: nuovo prodotto e aggiunta prodotto al carrello

---
+++ eMarketStall/features/new_product.feature
@@ -0,0 +1,14 @@
+Feature: Creating a new listing for selling a handmade creation
+
+Scenario: Seller creates a new listing
+    Given I am a logged in user and i'm in the  root page
+    When I click the "New product" link
+    And I fill in the title with "Handcrafted Necklace"
+    And I fill in the description with "Beautiful handcrafted necklace made with genuine gemstones."
+    And I set the price to "50.00"
+    And I set the category as "Collane"
+    And I set the availability to "3"
+    And I click the "Create Product" button
+    Then I should see a success message confirming the listing was created
+    And I should be redirected to the listing details page
+    And I should see the details of the newly created listing, including the title, description, price, category and availability
---
+++ eMarketStall/features/step_definitions/increasing_quantity.rb
@@ -0,0 +1,51 @@
+Given("I am a logged in user and have at least one product in the cart") do
+    # Implement the logic to simulate a registered seller
+    # Simulate a logged-in user which adds a new prodouct for sale
+    visit new_user_session_path
+    user = User.create(email: 'test.user@example.com', password: 'password', nome: 'Test', cognome: 'Bianchi', città: 'Padova')
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Accedi'
+    expect(page).to have_content('Signed in successfully.') # Expectation for success message
+    expect(page).to have_current_path(root_path)
+    click_link("New Product")
+    fill_in('product[title]', with: 'Product1')
+    fill_in('product[description]', with: 'Test')
+    fill_in('product[price]', with: '15')
+    select('Collane', from: 'product[category]')
+    fill_in('product[availability]', with: '2')
+    click_button("Create Product")
+    click_link("Logout")
+
+
+    visit new_user_session_path
+    user = User.create(email: 'test2.bianchi@example.com', password: 'password', nome: 'Marco', cognome: 'Rossi', città: 'Verona')
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Accedi'
+    expect(page).to have_content('Signed in successfully.') # Expectation for success message
+    expect(page).to have_current_path(root_path)
+    click_button('Add to cart')
+
+end
+
+When("I click the {string} link for the product") do |button_text|
+  @initial_quantity = CartItem.last.quantity.to_i
+  @intial_subtotal = CartItem.last.product.price
+  click_link(button_text)
+end
+
+# I know, it's not a good wat to do it but I noticed that in this context, every time I run cucumber, in this feature,
+# given the fact that two users are gonna log in, two carts would be created (we create a new cart every session), so for the second user
+# the correct route for going to his cart will be /carts/2
+Then("the quantity of the product in the cart should increase by 1") do
+    updated_quantity = CartItem.last.quantity.to_i
+    expect(updated_quantity).to eq(@initial_quantity + 1)
+end
+
+And("the order subtotal should be updated accordingly") do
+    expect(page).to have_content("Sub total: $#{CartItem.last.product.price + @intial_subtotal}0")
+end
+
+  
